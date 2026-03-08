c1216effe319b3a733b1fc1994c66171e95cc371
Riakol <inspi47@gmail.com>
2026-03-08 14:17:49 +0500

    Implement answer verification logic in MainActivity
    - Added initialization for UI components including example TextView, answer EditText, and submit Button.
    - Added logic to toggle visibility of 'Correct' and 'Incorrect' feedback TextViews based on the input validation result.

--- app/src/main/java/com/riakol/calculator/MainActivity.java
+++ app/src/main/java/com/riakol/calculator/MainActivity.java
@@ -1,12 +1,18 @@
 package com.riakol.calculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +25,26 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        TextView textViewExample = findViewById(R.id.textViewExample);
+        EditText editTextAnswer = findViewById(R.id.editTextAnswer);
+        Button buttonAnswer = findViewById(R.id.buttonAnswer);
+        TextView incorrectAnswer = findViewById(R.id.textViewIncorrectAnswer);
+        TextView correctAnswer = findViewById(R.id.textViewCorrectAnswer);
+
+        buttonAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = editTextAnswer.getText().toString();
+                if (text.equals("15")) {
+                    correctAnswer.setVisibility(View.VISIBLE);
+                    incorrectAnswer.setVisibility(View.GONE);
+                } else {
+                    correctAnswer.setVisibility(View.GONE);
+                    incorrectAnswer.setVisibility(View.VISIBLE);
+                }
+            }
         });
     }
 }
